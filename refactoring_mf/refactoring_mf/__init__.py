749153df0e4fa61f083013cbd39e57e90815aa74
horyujey <jeyjeyjeey@gmail.com>
2022-07-17 20:13:27 +0900

    fix: main structure

--- refactoring_mf/refactoring_mf/__init__.py
+++ refactoring_mf/refactoring_mf/__init__.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from refactoring_mf.apps.statement import Statement
 from refactoring_mf.repository.json_file import get_data_from_json
 
-if __name__ == "__main__":
+def main():
     parser = ArgumentParser()
     parser.add_argument("invoice_json_file_path")
     args = parser.parse_args()
@@ -12,3 +12,6 @@ if __name__ == "__main__":
         get_data_from_json(args.invoice_json_file_path),
         get_data_from_json("plays")
     )
+
+if __name__ == "__main__":
+    main()
